9d4c03453502872e58cfe5dbe3b30418609db6e1
shokohsc <shokohsc@gmail.com>
2023-11-26 21:19:44 +0100

    chore(): added sentry/glitchtip sourcemaps upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePluginFonts } from 'vite-plugin-fonts'
 import { fileURLToPath, URL } from "node:url";
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 
 
 // https://vitejs.dev/config/
@@ -23,6 +24,16 @@ export default defineConfig({
         prefetch: false,
         injectTo: 'head-prepend'
       },
+    }),
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG || "shokohsc",
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      url: "https://glitchtip.shokohsc.home",
+      release: {
+        name: process.env.POD_NAME,
+      }
     }),
   ],
   resolve: {
