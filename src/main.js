94c3a5e5a1dbcbb0a6ac0ac25b3005a133a733ee
shokohsc <shokohsc@gmail.com>
2024-08-09 18:33:55 +0200

    starting pwa

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router.js'
+import './registerServiceWorker'
 
 const pinia = createPinia()
 const app = createApp(App)
